61f7cf25fe412944bebfead9511efa865e8fabe0
Alex Bardashevsky <mijiekc@icloud.com>
2018-12-04 14:44:36 +0200

    Initial Commit

---
+++ HomeWork - Swift -Урок номер 35. UITableView Search./ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork - Swift -Урок номер 35. UITableView Search.
+//
+//  Created by Oleksandr Bardashevskyi on 12/4/18.
+//  Copyright © 2018 Oleksandr Bardashevskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
